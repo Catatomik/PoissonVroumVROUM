fc70fdf6d17c8c7b6b36f1c063d8e73a7532696d
Gael Valade-GA <gvalade@bordeaux-inp.fr>
2025-03-14 21:47:42 +0100

    feat(client)[cli] : begin cli

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "PoissonVroumVROUM")]
+#[command(version = "0.1")]
+#[command(about = "Here we start fishes", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    id: String,
+    #[arg(default_value_t = 12345)]
+    port: u16,
+}
+
+
 fn main() {
+    let cli = Cli::parse();
+
+    println!("id : {}", cli.id);
+    println!("port : {}", cli.port);
     println!("Hello, world!");
 }

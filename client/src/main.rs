7bd9b5cb6d7088472267589cfe70ecc02184b7d3
Catatomik <cata@catadev.org>
2025-03-21 11:47:24 +0100

    feat: simple main using FishAPI with TCP

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,48 @@
+mod network;
+mod view;
+
+use network::{api::FishApi, protocol::ServerPacket, tcp::TcpClient};
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    str::FromStr as _,
+    thread::sleep,
+    time::Duration,
+};
+
+struct App {
+    fish_api: FishApi<TcpClient>,
+}
+
+impl App {
+    pub fn handle_packet(&mut self, packet: ServerPacket) {
+        match packet {
+            network::protocol::ServerPacket::Pong => {
+                println!("Pong!")
+            }
+            _ => unimplemented!(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App {
+        fish_api: FishApi::new(TcpClient::new(SocketAddrV4::new(
+            Ipv4Addr::from_str("127.0.0.1").expect("Couldn't parse IPv4 address"),
+            12345,
+        ))),
+    };
+
+    loop {
+        match app.fish_api.try_receive() {
+            Ok(Some(res)) => app.handle_packet(res),
+            Ok(None) => {}
+            Err(e) => {
+                eprintln!("Error while treating Fish API response {:?}", e);
+            }
+        };
+
+        // Additional actions
+
+        sleep(Duration::from_millis(100));
+    }
 }

62f5c1de65d00f52115f7e4d18fe57e7bfcc409f
Faustine Lacroix <faustine.lacroix@enseirb-matmeca.fr>
2025-03-28 09:59:52 +0100

    [server_TCPconnexion] format main.c

--- server/src/main.c
+++ server/src/main.c
@@ -1,9 +1,9 @@
 #include "utils.h"
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#include"connexion.h"
+#include "connexion.h"
 #include "parse_cfg.h"
 
 int main(int argc, char **argv) {
@@ -11,26 +11,23 @@ int main(int argc, char **argv) {
     UNUSED(argv);
     pthread_t thread_connexion;
 
-    //init config structur of controller configuration file
+    // init config structur of controller configuration file
     struct config_t *config = malloc(sizeof(struct config_t));
-    if (config_from_file(config, "./controller.cfg")){
-      
-      //const char *controller_addr = "127.0.0.1";
-      //int controller_port = config.controller_port;    
-      //int display_timeout_value = config.display_timeout_value;
-      //int fish_update_interval = config.fish_update_interval;
+    if (config_from_file(config, "./controller.cfg")) {
 
-      pthread_create(&thread_connexion, NULL, start, (void*) config);
-      pthread_detach(thread_connexion);
-      while(1){}
+        // const char *controller_addr = "127.0.0.1";
+        // int controller_port = config.controller_port;
+        // int display_timeout_value = config.display_timeout_value;
+        // int fish_update_interval = config.fish_update_interval;
+
+        pthread_create(&thread_connexion, NULL, start, (void *)config);
+        pthread_detach(thread_connexion);
+        while (1) {
+        }
+    } else {
+        printf("Error while parsing config file");
+        return -1;
     }
-    else{
-      printf("Error while parsing config file");
-      return -1;
-    }
-    
-    
-    
 
     printf("Hello, World!\n");
     return 0;

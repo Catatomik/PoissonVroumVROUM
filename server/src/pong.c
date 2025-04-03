74ebaf1ff1c4cd1c722b7eb1b5cfde1ffae4c7e7
Faustine Lacroix <faustine.lacroix@enseirb-matmeca.fr>
2025-04-03 17:12:36 +0200

    [server_pong_proposal] feat pong.c, adapte function prototype and implement greeting function

--- server/src/pong.c
+++ server/src/pong.c
@@ -4,85 +4,79 @@
 
 #include "pong.h"
 
-// void help(char *args, char *server_response) {
-//     // general help
-//     if (parsed->argc == 1) {
-//         char *intro =
-//             "For more information about a cmd, do help <cmd>\nlist of "
-//             "commandes:\n\t> hello\n\t> getFish\n\t> ls\n\t> log\n\t>
-//             ping\n";
-//         strcpy(server_response, intro);
-//     }
+int greeting(char *args, size_t args_len, char *send_buffer,
+             size_t send_buffer_capacity) {
+    // extract id of the viewers
+    char *id = malloc(6 * sizeof(char));
+    if (id == NULL) {
+        printf("Error allocation memory\n");
+        return 1;
+    }
+    char *conf;
+    char *start_char_id = strchr(args, 'N');
 
-//     // help hello
-//     if (strcmp(parsed->argv[1], "hello") == 0) {
-//         char *hello = "hello------------------------------\nDescription: for
-//         a "
-//                       "client to identify itself to the server and retrieve "
-//                       "its settings\nExemple request: > hello in as "
-//                       "N3\nExemple response: < greeting N3 0x500+500+500\n";
-//         strcpy(server_response, hello);
-//     }
+    // no id precised
+    if (start_char_id == NULL) {
+        memcpy(id, "N2", 2);
+        id[2] = '\0';
+        conf = "500x0+500+500";
+    } else {
+        int start_index_id = start_char_id - args;
+        int end_index_id = start_index_id + 1;
+        while (args[end_index_id] == '1' || args[end_index_id] == '2' ||
+               args[end_index_id] == '3' || args[end_index_id] == '4' ||
+               args[end_index_id] == '5' || args[end_index_id] == '6' ||
+               args[end_index_id] == '7' || args[end_index_id] == '8' ||
+               args[end_index_id] == '9' || args[end_index_id] == '0') {
+            end_index_id += 1;
+        }
+        end_index_id -= start_index_id;
+        memcpy(id, args + start_index_id, end_index_id);
+        id[end_index_id] = '\0';
 
-//     // help getFish
-//     if (strcmp(parsed->argv[1], "getFish") == 0) {
-//         char *getFish =
-//             "getFish-----------------------------\nDescription: controller "
-//             "returns the list of fish to be managed by the display "
-//             "program\nExemple request: > getFishes\nExemple response:  <list
-//             "
-//             "[PoissonRouge at 90x4,10x4,5] [PoissonClown at 20x80,12x6,5]\n";
-//         strcpy(server_response, getFish);
-//     }
+        // give them settings
+        if (strncmp(id, "N1", 2) == 0)
+            conf = "0x0+500+500";
+        else if (strncmp(id, "N2", 2) == 0)
+            conf = "500x0+500+500";
+        else if (strncmp(id, "N3", 2) == 0)
+            conf = "0x500+500+500";
+        else if (strncmp(id, "N4", 2) == 0)
+            conf = "500x500+500+500";
+        else {
+            snprintf(send_buffer, send_buffer_capacity, "no greeting\n");
+            free(id);
+            return 0;
+        }
+    }
 
-//     // help ls
-//     if (strcmp(parsed->argv[1], "ls") == 0) {
-//         char *ls =
-//             "ls--------------------------------\nDescription: controller "
-//             "returns the list of fish to be continuously managed by the "
-//             "display program\nExemple request: > ls\nExemple response: < list
-//             "
-//             "[PoissonRouge at 92x40,10x4,5] [PoissonClown at 22x80,12x6,5]\n<
-//             " "list [PoissonRouge at 70x40,10x4,15] [PoissonClown at "
-//             "10x90,12x6,6]\n< list [PoissonRouge at 30x30,10x4,8] "
-//             "[PoissonClown at 30x30,12x6,11]";
-//         strcpy(server_response, ls);
-//     }
+    // greeting
+    int printed_count = snprintf(send_buffer, send_buffer_capacity,
+                                 "greting %s %s\n", id, conf);
+    free(id);
+    if (printed_count > send_buffer_capacity) {
+        fprintf(
+            stderr,
+            "[ERR] response would have overran send_buffer (len=%zu) because "
+            "of arg len=%d\n",
+            send_buffer_capacity, printed_count);
+        return -1;
+    }
+    return 0;
+}
 
-//     // help log
-//     if (strcmp(parsed->argv[1], "log") == 0) {
-//         char *log =
-//             "log------------------------------\nDescription: "
-//             "deconnexion\nExemple request: > log out\nExemple response: <
-//             bye";
-//         strcpy(server_response, log);
-//     }
-
-//     //-------------------------------------------
-//     if (strcmp(parsed->argv[1], "ping") == 0) {
-//         char *ping = "ping----------------------------\nDescription: Keep "
-//                      "connexion alive\nExemple request: > ping 12345\nExemple
-//                      " "response: < pong 12345";
-//         strcpy(server_response, ping);
-//     }
-
-//     else {
-//         char *error = "no cmd corespond to what you wrote, maybe you
-//         miswrite!"; strcpy(server_response, error);
-//     }
-
-//     //...
-// }
-// void greeting(char *args, char *server_response) {
-//     strcpy(server_response, "not yet greeting");
-// }
-// void list(char *args, char *server_response) {
+// void list(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity) {
 //     strcpy(server_response, "not yet list");
 // }
-// void listls(char *args, char *server_response) {
+
+// void listls(char *args, size_t args_len, char *send_buffer,size_t
+// send_buffer_capacity) {
 //     strcpy(server_response, "not yet listls");
 // }
-// void bye(char *args, char *server_response) {
+
+// void bye(char *args, size_t args_len, char *send_buffer,size_t
+// send_buffer_capacity) {
 //     strcpy(server_response, "not yet bye");
 // }
 
@@ -101,13 +95,16 @@ int pong(char *args, size_t args_len, char *send_buffer,
     return 0;
 }
 
-// void responseToAdd(char *args, char *server_response) {
+// void responseToAdd(char *args, size_t args_len, char *send_buffer,size_t
+// send_buffer_capacity) {
 //     strcpy(server_response, "not yet responseToAdd");
 // }
-// void responseToDel(char *args, char *server_response) {
+// void responseToDel(char *args, size_t args_len, char *send_buffer,size_t
+// send_buffer_capacity) {
 //     strcpy(server_response, "not yet responseToDel");
 // }
-// void responseToStrat(char *args, char *server_response) {
+// void responseToStrat(char *args, size_t args_len, char *send_buffer,size_t
+// send_buffer_capacity) {
 //     strcpy(server_response, "not yet responseToStart");
 // }
 
@@ -115,34 +112,44 @@ int handle_client_request(char *receive_buffer, size_t receive_buffer_len,
                           char *send_buffer, size_t send_buffer_capacity) {
     // printf("%s\n", receive_buffer);
 
-    // if (strcmp(parsed->argv[0], "help") == 0) {
-    //     return help(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "hello") == 0) {
-    //     return greeting(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "getFish") == 0) {
-    //     return list(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "ls") == 0) {
-    //     return listls(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "log") == 0) {
-    //     return bye(parsed, server_response);
-    // }
+    /* if (strncmp(receive_buffer, "help", 4) == 0) { */
+    /*   return help(parsed, server_response); */
+    /* } */
+    if (strncmp(receive_buffer, "hello", 4) == 0) {
+        return greeting(receive_buffer + 5, receive_buffer_len, send_buffer,
+                        send_buffer_capacity);
+    }
+    /* if (strncmp(receive_buffer, "getFish", 7) == 0) { */
+    /*   return list(receive_buffer + 8, receive_buffer_len, send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
+    /* if (strncmp(receive_buffer, "ls", 2) == 0) { */
+    /*   return listls(receive_buffer + 3, receive_buffer_len, send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
+    /* if (strncmp(receive_buffer, "log", 3) == 0) { */
+    /*   return bye(receive_buffer + 4, receive_buffer_len, send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
     if (strncmp(receive_buffer, "ping", 4) == 0) {
-        return pong(receive_buffer + 4, receive_buffer_len, send_buffer,
+        return pong(receive_buffer + 5, receive_buffer_len, send_buffer,
                     send_buffer_capacity);
     }
-    // if (strcmp(parsed->argv[0], "addFish") == 0) {
-    //     return responseToAdd(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "delFish") == 0) {
-    //     return responseToDel(parsed, server_response);
-    // }
-    // if (strcmp(parsed->argv[0], "startFish") == 0) {
-    //     return responseToStrat(parsed, server_response);
-    // }
+    /* if (strncmp(receive_buffer, "addFish", 7) == 0) { */
+    /*   return responseToAdd(receive_buffer + 8, receive_buffer_len,
+     * send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
+    /* if (strncmp(receive_buffer, "delFish", 7) == 0) { */
+    /*   return responseToDel(receive_buffer + 8, receive_buffer_len,
+     * send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
+    /* if (strncmp(receive_buffer, "startFish", 9) == 0) { */
+    /*   return responseToStrat(receive_buffer + 10, receive_buffer_len,
+     * send_buffer, */
+    /* 		send_buffer_capacity); */
+    /* } */
 
     fprintf(stderr, "[ERR] unknown protocol command\n");
     return -1;
--- server/src/pong.h
+++ server/src/pong.h
@@ -9,16 +9,24 @@ struct parse {
     char *argv[256];
 };
 
-// int help(char *args, char *server_response);
-// int greeting(char *args, char *server_response);
-// int list(char *args, char *server_response);
-// int listls(char *args, char *server_response);
-// int bye(char *args, char *server_response);
+// int help(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int greeting(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int list(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int listls(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int bye(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
 int pong(char *args, size_t args_len, char *send_buffer,
          size_t send_buffer_capacity);
-// int responseToAdd(char *args, char *server_response);
-// int responseToDel(char *args, char *server_response);
-// int responseToStrat(char *args, char *server_response);
+// int responseToAdd(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int responseToDel(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
+// int responseToStrat(char *args, size_t args_len, char *send_buffer,
+//          size_t send_buffer_capacity);
 
 int handle_client_request(char *receive_buffer, size_t receive_buffer_len,
                           char *send_buffer, size_t send_buffer_capacity);

d376c8ffb7e13890bd942b0b649a734f60fcfa9e
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-04-04 08:42:04 +0200

    fix: parse_cfg

--- server/tests/parse_cfg.c
+++ server/tests/parse_cfg.c
@@ -1,14 +1,11 @@
 #include "parse_cfg.h"
 #include "utils.h"
+#include <assert.h>
 #include <stdio.h>
 
 void test_config_parsing() {
     struct config_t cfg;
-    config_from_file(&cfg, "./controller.cfg");
-    printf("config (controller_port=%d, display_timeout_value=%d, "
-           "fish_update_interval=%d)\n",
-           cfg.controller_port, cfg.display_timeout_value,
-           cfg.fish_update_interval);
+    assert(config_from_file(&cfg, "./controller.cfg") == 0);
 }
 
 int main(int argc, char **argv) {
